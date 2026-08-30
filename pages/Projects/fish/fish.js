4a8284301543e0aac9bfed14c6689b8d1af70a82
Eslam Adel <eslam.adel2596@gmail.com>
2026-08-30 14:20:36 +0300

    Add live project previews and improve Projects showcase

--- pages/Projects/fish/fish.js
+++ pages/Projects/fish/fish.js
@@ -1,7 +1,12 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 0.1, 500);
+var camera = new THREE.PerspectiveCamera(
+  60,
+  innerWidth / innerHeight,
+  0.1,
+  500,
+);
 camera.position.set(0, -25, 80);
-var renderer = new THREE.WebGLRenderer({antialias: true});
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor(0x181005);
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,11 +19,11 @@ controls.enableDamping = true;
 
 let geoms = [];
 let pts = [
-  new THREE.Vector2(0, 1. - 0),
-  new THREE.Vector2(0.25, 1. - 0),
-  new THREE.Vector2(0.25, 1. - 0.125),
-  new THREE.Vector2(0.45, 1. - 0.125),
-  new THREE.Vector2(0.45, 1. - 0.95)
+  new THREE.Vector2(0, 1 - 0),
+  new THREE.Vector2(0.25, 1 - 0),
+  new THREE.Vector2(0.25, 1 - 0.125),
+  new THREE.Vector2(0.45, 1 - 0.125),
+  new THREE.Vector2(0.45, 1 - 0.95),
 ];
 var geom = new THREE.LatheBufferGeometry(pts, 20);
 geoms.push(geom);
@@ -35,23 +40,32 @@ var num = 500;
 let instPos = []; //3
 let instSpeed = []; //1
 let instLight = []; // 2 (initial intensity, frequency)
-for (let i = 0; i < num; i++){
-  instPos.push( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
-  instSpeed.push( Math.random() * 0.25 + 1);
-  instLight.push( Math.PI + (Math.PI * Math.random()), Math.random() + 5);
+for (let i = 0; i < num; i++) {
+  instPos.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+  instSpeed.push(Math.random() * 0.25 + 1);
+  instLight.push(Math.PI + Math.PI * Math.random(), Math.random() + 5);
 }
-instGeom.setAttribute("instPos", new THREE.InstancedBufferAttribute(new Float32Array(instPos), 3));
-instGeom.setAttribute("instSpeed", new THREE.InstancedBufferAttribute(new Float32Array(instSpeed), 1));
-instGeom.setAttribute("instLight", new THREE.InstancedBufferAttribute(new Float32Array(instPos), 2));
+instGeom.setAttribute(
+  "instPos",
+  new THREE.InstancedBufferAttribute(new Float32Array(instPos), 3),
+);
+instGeom.setAttribute(
+  "instSpeed",
+  new THREE.InstancedBufferAttribute(new Float32Array(instSpeed), 1),
+);
+instGeom.setAttribute(
+  "instLight",
+  new THREE.InstancedBufferAttribute(new Float32Array(instLight), 2),
+);
 
 var mat = new THREE.ShaderMaterial({
   uniforms: {
-    uTime: {value: 0},
-    uLight: {value: new THREE.Color("red").multiplyScalar(1.5)},
-    uColor: {value: new THREE.Color("maroon").multiplyScalar(1)},
-    uFire: {value: new THREE.Color(1, 0.75, 0)}
+    uTime: { value: 0 },
+    uLight: { value: new THREE.Color("red").multiplyScalar(1.5) },
+    uColor: { value: new THREE.Color("maroon").multiplyScalar(1) },
+    uFire: { value: new THREE.Color(1, 0.75, 0) },
   },
-  vertexShader:`
+  vertexShader: `
     uniform float uTime;
 
     attribute vec3 instPos;
@@ -103,7 +117,7 @@ var mat = new THREE.ShaderMaterial({
       gl_FragColor = vec4(col, 1);
     }
 `,
-  side: THREE.DoubleSide
+  side: THREE.DoubleSide,
 });
 
 var lantern = new THREE.Mesh(instGeom, mat);
@@ -115,8 +129,7 @@ let oUs = [];
 
 let loader = new THREE.STLLoader();
 //https://clara.io/view/b47726c8-02cf-4eb5-b275-d9b2be591bad
-loader.load("https://cywarr.github.io/small-shop/fish.stl", objGeom => {
-  console.log(objGeom);
+loader.load("https://cywarr.github.io/small-shop/fish.stl", (objGeom) => {
   //objGeom.rotateX(-MathPI * 0.5);
 
   // path
@@ -124,67 +137,85 @@ loader.load("https://cywarr.github.io/small-shop/fish.stl", objGeom => {
   let axis = new THREE.Vector3(0, 1, 0);
   let cPts = [];
   let cSegments = 6;
-  let cStep = Math.PI * 2 / cSegments;
-  for (let i = 0; i < cSegments; i++){
+  let cStep = (Math.PI * 2) / cSegments;
+  for (let i = 0; i < cSegments; i++) {
     cPts.push(
-      new THREE.Vector3().copy(baseVector)
-      //.setLength(35 + (Math.random() - 0.5) * 5)
-      .applyAxisAngle(axis, cStep * i).setY(THREE.MathUtils.randFloat(-10, 10))
+      new THREE.Vector3()
+        .copy(baseVector)
+        //.setLength(35 + (Math.random() - 0.5) * 5)
+        .applyAxisAngle(axis, cStep * i)
+        .setY(THREE.MathUtils.randFloat(-10, 10)),
     );
   }
   let curve = new THREE.CatmullRomCurve3(cPts);
   curve.closed = true;
 
-  console.log(curve);
-
   let numPoints = 511;
   let cPoints = curve.getSpacedPoints(numPoints);
   let cObjects = curve.computeFrenetFrames(numPoints, true);
-  console.log(cObjects);
   let pGeom = new THREE.BufferGeometry().setFromPoints(cPoints);
-  let pMat = new THREE.LineBasicMaterial({color: "yellow"});
+  let pMat = new THREE.LineBasicMaterial({ color: "yellow" });
   let pathLine = new THREE.Line(pGeom, pMat);
   //scene.add(pathLine);
 
   // data texture
   let data = [];
-  cPoints.forEach( v => { data.push(v.x, v.y, v.z);} );
-  cObjects.binormals.forEach( v => { data.push(v.x, v.y, v.z);} );
-  cObjects.normals.forEach( v => { data.push(v.x, v.y, v.z);} );
-  cObjects.tangents.forEach( v => { data.push(v.x, v.y, v.z);} );
+  cPoints.forEach((v) => {
+    data.push(v.x, v.y, v.z);
+  });
+  cObjects.binormals.forEach((v) => {
+    data.push(v.x, v.y, v.z);
+  });
+  cObjects.normals.forEach((v) => {
+    data.push(v.x, v.y, v.z);
+  });
+  cObjects.tangents.forEach((v) => {
+    data.push(v.x, v.y, v.z);
+  });
 
   let dataArray = new Float32Array(data);
 
-  let tex = new THREE.DataTexture(dataArray, numPoints + 1, 4, THREE.RGBFormat, THREE.FloatType);
+  let tex = new THREE.DataTexture(
+    dataArray,
+    numPoints + 1,
+    4,
+    THREE.RGBFormat,
+    THREE.FloatType,
+  );
   tex.magFilter = THREE.NearestFilter;
-  console.log(tex);
-  
+
   objGeom.center();
   objGeom.rotateX(-Math.PI * 0.5);
   objGeom.scale(0.5, 0.5, 0.5);
-  let objBox = new THREE.Box3().setFromBufferAttribute(objGeom.getAttribute("position"));
+  let objBox = new THREE.Box3().setFromBufferAttribute(
+    objGeom.getAttribute("position"),
+  );
   let objSize = new THREE.Vector3();
   objBox.getSize(objSize);
   //objGeom.translate(0, 0, objBox.z);
 
   objUniforms = {
-    uSpatialTexture: {value: tex},
-    uTextureSize: {value: new THREE.Vector2(numPoints + 1, 4)},
-    uTime: {value: 0},
-    uLengthRatio: {value: objSize.z / curve.cacheArcLengths[200]}, // more or less real lenght along the path
-    uObjSize: {value: objSize} // lenght
-  }
+    uSpatialTexture: { value: tex },
+    uTextureSize: { value: new THREE.Vector2(numPoints + 1, 4) },
+    uTime: { value: 0 },
+    uLengthRatio: { value: objSize.z / curve.cacheArcLengths[200] }, // more or less real lenght along the path
+    uObjSize: { value: objSize }, // lenght
+  };
   oUs.push(objUniforms);
 
-  let objMat = new THREE.MeshBasicMaterial({color: 0xff6600, wireframe: true});
-  objMat.onBeforeCompile = shader => {
+  let objMat = new THREE.MeshBasicMaterial({
+    color: 0xff6600,
+    wireframe: true,
+  });
+  objMat.onBeforeCompile = (shader) => {
     shader.uniforms.uSpatialTexture = objUniforms.uSpatialTexture;
     shader.uniforms.uTextureSize = objUniforms.uTextureSize;
     shader.uniforms.uTime = objUniforms.uTime;
     shader.uniforms.uLengthRatio = objUniforms.uLengthRatio;
     shader.uniforms.uObjSize = objUniforms.uObjSize;
 
-    shader.vertexShader = `
+    shader.vertexShader =
+      `
       uniform sampler2D uSpatialTexture;
       uniform vec2 uTextureSize;
       uniform float uTime;
@@ -233,14 +264,13 @@ loader.load("https://cywarr.github.io/small-shop/fish.stl", objGeom => {
       vec3 N = mix(splinePrev.normal, splineNext.normal, f);
 
       transformed = P + (N * pos.x) + (B * pos.y);
-  `
+  `,
     );
     //console.log(shader.vertexShader);
-  }
+  };
   let obj = new THREE.Mesh(objGeom, objMat);
   scene.add(obj);
 });
-
 
 var clock = new THREE.Clock();
 
@@ -248,6 +278,8 @@ renderer.setAnimationLoop(() => {
   controls.update();
   let t = clock.getElapsedTime();
   mat.uniforms.uTime.value = t;
-  oUs.forEach(ou => {ou.uTime.value = t;});
+  oUs.forEach((ou) => {
+    ou.uTime.value = t;
+  });
   renderer.render(scene, camera);
 });
